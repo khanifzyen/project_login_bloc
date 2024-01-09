6601830f5e42fe5e6fb510cf766fadf9976f76a9
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2024-01-09 16:26:27 +0700

    login page convert to stateful

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool isVisible = true;
+
+  void toggleVisible() {
+    setState(() {
+      isVisible = !isVisible;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +33,7 @@ class LoginPage extends StatelessWidget {
               decoration: const InputDecoration(
                 labelText: "Email",
                 hintText: "Enter your email",
+                hintStyle: TextStyle(color: Color.fromARGB(255, 175, 175, 175)),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(Radius.circular(10.0)),
                   borderSide: BorderSide(color: Colors.blue),
@@ -31,15 +44,21 @@ class LoginPage extends StatelessWidget {
             const SizedBox(height: 10),
             TextField(
                 controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
+                obscureText: isVisible,
+                decoration: InputDecoration(
                   labelText: "Password",
                   hintText: "Enter your password",
-                  border: OutlineInputBorder(
+                  hintStyle: const TextStyle(
+                      color: Color.fromARGB(255, 175, 175, 175)),
+                  border: const OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10.0)),
                     borderSide: BorderSide(color: Colors.blue),
                   ),
-                  suffixIcon: Icon(Icons.visibility_off),
+                  suffixIcon: IconButton(
+                      onPressed: toggleVisible,
+                      icon: isVisible
+                          ? const Icon(Icons.visibility)
+                          : const Icon(Icons.visibility_off)),
                 )),
             const SizedBox(height: 10),
             ElevatedButton(onPressed: () {}, child: const Text("Login"))
