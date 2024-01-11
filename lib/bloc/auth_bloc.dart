58e2aee26285c0a087c024691777f425c00ec242
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2024-01-11 14:22:22 +0700

    extract event handler constructor

--- lib/bloc/auth_bloc.dart
+++ lib/bloc/auth_bloc.dart
@@ -6,46 +6,48 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequested>(
-      (event, emit) async {
-        emit(AuthLoading());
-        try {
-          final email = event.email;
-          final password = event.password;
+    on<AuthLoginRequested>(_onAuthLoginRequested);
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
+  }
 
-          //taruh kode untuk format email menggunakan regex
-          if (!RegExp(
-                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-              .hasMatch(email)) {
-            return emit(AuthFailure('Email tidak valid'));
-          }
-          //taruh kode untuk password minimal 6 dan terdiri dari huruf besar, huruf kecil, angka, dan simbol
-          if (!RegExp(
-                  r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$")
-              .hasMatch(password)) {
-            return emit(AuthFailure(
-                'Password minimal 6 huruf, terdiri dari huruf besar, huruf kecil, angka dan simbol'));
-          }
+  void _onAuthLoginRequested(
+      AuthLoginRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final email = event.email;
+      final password = event.password;
 
-          await Future.delayed(const Duration(seconds: 1), () {
-            return emit(AuthSuccess(uid: '$email-$password'));
-          });
-        } catch (e) {
-          return emit(AuthFailure(e.toString()));
-        }
-      },
-    );
-    on<AuthLogoutRequested>(
-      (event, emit) async {
-        emit(AuthLoading());
-        try {
-          await Future.delayed(const Duration(seconds: 1), () {
-            return emit(AuthInitial());
-          });
-        } catch (e) {
-          return emit(AuthFailure(e.toString()));
-        }
-      },
-    );
+      //taruh kode untuk format email menggunakan regex
+      if (!RegExp(
+              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+          .hasMatch(email)) {
+        return emit(AuthFailure('Email tidak valid'));
+      }
+      //taruh kode untuk password minimal 6 dan terdiri dari huruf besar, huruf kecil, angka, dan simbol
+      if (!RegExp(
+              r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$")
+          .hasMatch(password)) {
+        return emit(AuthFailure(
+            'Password minimal 6 huruf, terdiri dari huruf besar, huruf kecil, angka dan simbol'));
+      }
+
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthSuccess(uid: '$email-$password'));
+      });
+    } catch (e) {
+      return emit(AuthFailure(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(
+      AuthLogoutRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthInitial());
+      });
+    } catch (e) {
+      return emit(AuthFailure(e.toString()));
+    }
   }
 }
